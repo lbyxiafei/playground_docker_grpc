bdb8c06c489a82406c389668239028a00c6cdded
Binyan Li <binyanli@microsoft.com>
2022-09-14 22:54:47 -0700

    Onboard helloworld server.

---
+++ helloworld_server.py
@@ -0,0 +1,26 @@
+from concurrent import futures
+
+import sys
+sys.path.append('.')
+
+import logging
+
+import grpc
+from protos.generated import helloworld_pb2, helloworld_pb2_grpc
+
+class HelloWorldServer(helloworld_pb2_grpc.GreeterServicer):
+
+    def SayHello(self, request, context):
+        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(HelloWorldServer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
